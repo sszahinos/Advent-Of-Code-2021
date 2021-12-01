7698571874ac52a52099c8b85f88f04c7aabc69b
sszahinos <sergiosaza6@gmail.com>
2021-12-01 16:27:38 +0100

    Created branch for sol2

--- 01_sonar/sonar.py
+++ 01_sonar/sonar.py
@@ -6,12 +6,18 @@ if len(sys.argv) != 2:
 
 inputFile = open(sys.argv[1], "r")
 measures = inputFile.readlines()
+#Formatting array
+for index, measure in enumerate(measures):
+    measures[index] = int(measure.rstrip("\n"))
+
 auxPre = 0
 incCount = -1
 decCount = 0
 
+sumPre = 0
+
 for index, measure in enumerate(measures):
-    if int(measure.rstrip("\n")) > auxPre:
+    if int(measure.rstrip("\n")) + int(measures[index+1].rstrip("\n"))> auxPre:
         incCount += 1
     else:
         decCount += 1
