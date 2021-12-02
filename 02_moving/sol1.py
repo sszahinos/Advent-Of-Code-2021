0391ca5eefb184c71729538bb865e5d25878aa34
sszahinos <sergiosaza6@gmail.com>
2021-12-02 12:10:09 +0100

    Solution 1

--- 02_moving/sol1.py
+++ 02_moving/sol1.py
@@ -6,13 +6,38 @@ if len(sys.argv) != 2:
 
 #############
 
+FORWARD = "forward"
+UP = "up"
+DOWN = "down"
+
 horizontal = 0
 depth = 0
+
+def goUp(distance):
+    global depth
+    depth -= distance
+
+def goDown(distance):
+    global depth
+    depth += distance
+
+def goForward(distance):
+    global horizontal
+    horizontal += distance
 
 inputFile = open(sys.argv[1], "r")
 steps = inputFile.readlines()
 
 #Formatting array
 for index, step in enumerate(steps):
-    steps[index] = int(step.rstrip("\n"))
+    steps[index] = step.rstrip("\n")
+    steps[index] = steps[index].split(" ")
+    if steps[index][0] == FORWARD:
+        goForward(int(steps[index][1]))
+    elif steps[index][0] == UP:
+        goUp(int(steps[index][1]))
+    elif steps[index][0] == DOWN:
+        goDown(int(steps[index][1]))
 
+print("Distance: {}\nDepth: {}".format(horizontal, depth))
+print("Puzzle answer: {}".format(horizontal*depth))
