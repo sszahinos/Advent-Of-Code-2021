7c0f4308bc3bdb4fd96c7efe16e159bd6c23e0b3
sszahinos <sergiosaza6@gmail.com>
2021-12-06 12:09:00 +0100

    day 5 skel

---
+++ 05_hydrothermal_venture/sol1.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Error: Pass one input file as argument.")
+    sys.exit()
+
+#############
+
+inputFile = open(sys.argv[1], "r")
+lines = inputFile.readlines()
+
+
+lines[0].rstrip("\n").split(",")
