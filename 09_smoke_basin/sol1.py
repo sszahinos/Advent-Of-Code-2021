e2575ad8e2feae4da0c8cb88015da65d0ee11f5e
sszahinos <sergiosaza6@gmail.com>
2021-12-11 21:42:07 +0100

    Day 8 skeleton

---
+++ 09_smoke_basin/sol1.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Error: Pass one input file as argument.")
+    sys.exit()
+
+#############
+
+inputFile = open(sys.argv[1], "r")
+inputs = inputFile.readlines()
+
+for index, input in enumerate(inputs):
+    inputs[index] = inputs[index].rstrip("\n").split(" | ")
