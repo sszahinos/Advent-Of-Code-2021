4c77a5e704339e5d37fa61cf709cfadc2b5a8e26
sszahinos <sergiosaza6@gmail.com>
2021-12-11 17:11:01 +0100

    sol1

--- 07_whale_attack/sol1.py
+++ 07_whale_attack/sol1.py
@@ -7,6 +7,20 @@ if len(sys.argv) != 2:
 #############
 
 inputFile = open(sys.argv[1], "r")
-fishes = inputFile.readlines()[0].split(",")
-for index, item in enumerate(fishes):
-    fishes[index] = int(item)
+crabs = inputFile.readlines()[0].split(",")
+for index, item in enumerate(crabs):
+    crabs[index] = int(item)
+
+minFuel = None
+minPos = None
+
+for pos in crabs:
+    fuel = 0
+    for crab in crabs:
+        fuel += abs(crab-pos)
+    
+    if minFuel == None or fuel < minFuel:
+        minFuel = fuel
+        minPos = pos
+
+print("Position: {} Fuel: {}".format(minPos, minFuel))
