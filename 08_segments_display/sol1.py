03b7f43fa076d04809fcc0c42d33191f342f17d5
sszahinos <sergiosaza6@gmail.com>
2021-12-11 18:10:28 +0100

    day 08 skeleton

---
+++ 08_segments_display/sol1.py
@@ -0,0 +1,12 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Error: Pass one input file as argument.")
+    sys.exit()
+
+#############
+
+inputFile = open(sys.argv[1], "r")
+crabs = inputFile.readlines()[0].split(",")
+for index, item in enumerate(crabs):
+    crabs[index] = int(item)
