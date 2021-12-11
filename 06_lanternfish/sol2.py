1645f3e77b8a01824baeea6ddabf8b1af8337eb3
sszahinos <sergiosaza6@gmail.com>
2021-12-11 13:27:51 +0100

    Sol2

--- 06_lanternfish/sol2.py
+++ 06_lanternfish/sol2.py
@@ -12,51 +12,57 @@ for index, item in enumerate(fishes):
     fishes[index] = int(item)
 
 ###
-DAYS = 5
+DAYS = 256
 NEW_FISH = 8
 DEFAULT_FISH = 6
 
 daysLeftFishes = []
+dayChanges = []
+
+def initializeChanges(changes):
+    for index, change in enumerate(changes):
+        changes[index] = 0
+
+    return changes
+
+
+def getTotalFishes(fishes):
+    totalFishes = 0
+
+    for fish in fishes:
+        totalFishes += fish
+
+    return totalFishes
+
+###
+
 for maxDays in range(NEW_FISH+1):
     daysLeftFishes.append(0)
+    dayChanges.append(0)
 
 for fish in fishes:
     daysLeftFishes[fish] += 1
+daysLeftFishes = daysLeftFishes[::-1]
 print("Initial: {}".format(daysLeftFishes))
 
 for day in range(DAYS):
-    for index, fishes in enumerate(reversed(daysLeftFishes)):
-        if daysLeftFishes[index] != 0:
-            print("PRE {}".format(fishes))
-            daysLeftFishes[index] -= 1
-            print("post {}".format(daysLeftFishes[index]))
+    for index, fishes in enumerate(daysLeftFishes): 
+        if index != len(daysLeftFishes)-1 and fishes > 0: # > 0 days
+            dayChanges[index+1] += fishes
+            dayChanges[index] -= fishes
+        elif fishes > 0: #0 days
+            dayChanges[0] += fishes
+            dayChanges[index] -= fishes
+            dayChanges[-DEFAULT_FISH-1] += fishes
 
-        if daysLeftFishes[index] != 0 and index != len(daysLeftFishes)-1:
-            daysLeftFishes[index+1] += 1
-        elif daysLeftFishes[index] != 0 and index == len(daysLeftFishes)-1: #0
-            daysLeftFishes[len(daysLeftFishes)-1] += 1
+    #END DAY
+    #Apply changes at the end of the day
+    for index, fishes in enumerate(daysLeftFishes):
+        daysLeftFishes[index] += dayChanges[index]
+    #Reset changes
+    initializeChanges(dayChanges)
 
-    print(daysLeftFishes)
-            
-        
-
-
-
-
-
-    #fishesToAdd = 0
-    #for index, fish in enumerate(fishes):
-    #    if fish == 0:
-            #New fish
-            #fishesToAdd += 1
-            #Reset old
-    #        fishes[index] = DEFAULT_FISH
-    #    else:
-    #        fishes[index] -= 1
-    
-    #for newFishes in range(fishesToAdd):
-        #fishes.append(NEW_FISH)
-
-#print("Days: {}".format(day))
-#print("Fish count: {}".format(totalFishes))
+print("Days: {}".format(DAYS))
+print(daysLeftFishes)
+print("Fish count: {}".format(getTotalFishes(daysLeftFishes)))
 
