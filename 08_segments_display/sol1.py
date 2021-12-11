ea02663c3f96dd58594797866a0159ed74896e2b
sszahinos <sergiosaza6@gmail.com>
2021-12-11 18:45:02 +0100

    Sol 1

--- 08_segments_display/sol1.py
+++ 08_segments_display/sol1.py
@@ -12,10 +12,51 @@ inputs = inputFile.readlines()
 for index, input in enumerate(inputs):
     inputs[index] = inputs[index].rstrip("\n").split(" | ")
 
-#[[input][code], ...]
+#[ [ [input][code] ], [ [input2][code2] ], ...]
 for index1, input in enumerate(inputs):
     for index2, part in enumerate(input):
         inputs[index1][index2] = inputs[index1][index2].split(" ")
 
 ###
 
+CODES = [
+    "abcefg", #0
+    "cf", #1
+    "acdeg", #2
+    "acdfg", #3
+    "bcdf", #4
+    "abdfg", #5
+    "abdefg", #6
+    "acf", #7
+    "abcdefg", #8
+    "abcdfg" #9
+]
+
+UNIQUE_CODES_LENGTHS = [
+    len(CODES[1]),
+    len(CODES[4]),
+    len(CODES[7]),
+    len(CODES[8])
+]
+
+totalUniques = 0
+
+def countUniques(codes):
+    count = 0
+    for code in codes:
+        if isUnique(code):
+            count += 1
+    return count
+
+def isUnique(code):
+    result = False
+    if len(code) in UNIQUE_CODES_LENGTHS:
+        result = True
+    return result
+
+
+
+for input in inputs:
+    totalUniques += countUniques(input[1])
+
+print("Total: {}".format(totalUniques))
