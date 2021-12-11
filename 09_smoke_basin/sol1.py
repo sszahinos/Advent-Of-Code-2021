8e797228b7b10247a242da0e03ebd0afeb8c714e
sszahinos <sergiosaza6@gmail.com>
2021-12-11 21:50:39 +0100

    input treatment

--- 09_smoke_basin/sol1.py
+++ 09_smoke_basin/sol1.py
@@ -10,4 +10,10 @@ inputFile = open(sys.argv[1], "r")
 inputs = inputFile.readlines()
 
 for index, input in enumerate(inputs):
-    inputs[index] = inputs[index].rstrip("\n").split(" | ")
+    inputs[index] = list(inputs[index].rstrip("\n"))
+    for index2, data in enumerate(inputs[index]):
+        inputs[index][index2] = int(data)
+
+###
+
+
